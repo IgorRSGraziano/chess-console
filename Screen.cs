2868d707e6ee0104ccdc83c8debd5f24d97bd847
igor.raphael <igor.raphael@openk.com.br>
2022-06-06 15:00:20 -0300

    style(chessboard): improve map and colors of pieces

--- Screen.cs
+++ Screen.cs
@@ -9,12 +9,41 @@ namespace Chess.Board
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Collums; j++)
                 {
-                    Console.Write(board.Piece(i, j) == null ? "- " : board.Piece(i, j) + " ");
+                    if (board.Piece(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine("");
             }
+            System.Console.WriteLine("* a b c d e f g h");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+            else if (piece.Color == Color.Black)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
