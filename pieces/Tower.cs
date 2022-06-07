88ab332e6985ea1d4c73c9ca38b64ebb1986fffc
igor.raphael <igor.raphael@openk.com.br>
2022-06-07 15:37:01 -0300

    feat(tower): add possible movements)

--- pieces/Tower.cs
+++ pieces/Tower.cs
@@ -10,5 +10,41 @@ namespace Chess.Pieces
         {
             return "T";
         }
+
+        private bool CanMoveTo(Position pos)
+        {
+            Piece p = Chessboard.Piece(pos);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            bool[,] matrix = new bool[Chessboard.Rows, Chessboard.Collums];
+
+            Position pos = new Position(0, 0);
+
+            void testingPosition(int difLine, int difCollum)
+            {
+                pos.SetValues(Position.Row + difLine, Position.Collum + difCollum);
+                while (Chessboard.IsValidPosition(pos) && CanMoveTo(pos))
+                {
+                    matrix[pos.Row, pos.Collum] = true;
+
+                    if (Chessboard.Piece(pos) != null && Chessboard.Piece(pos).Color != Color)
+                        break;
+
+                    pos.Row += difLine;
+                    pos.Collum += difCollum;
+
+                }
+            }
+
+            testingPosition(-1, 0); //Top
+            testingPosition(0, 1); //Right
+            testingPosition(1, 0); //Down
+            testingPosition(0, -1); //Left
+
+            return matrix;
+        }
     }
 }
